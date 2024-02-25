b84ce6bce01e51f01cf17f85dd233d8f02c84369
joseph kiragu <kiragz@gmail.com>
2024-02-25 14:04:11 +1030

    Addedd online chart logic including API, tested with postgre, yet to add file for database creation(I created it manually on my end)

---
+++ online chart/api.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'json'
+require 'sinatra/cross_origin'
+
+require_relative 'db_init'
+require_relative 'models/chat_message'
+
+register Sinatra::CrossOrigin
+
+configure do
+	enable :cross_origin # enable cross_origin
+end
+
+
+
+
+# Endpoint for chat history
+get '/chat-history' do
+	cross_origin
+	content_type :json
+  
+	# Fetch messages from the database
+	messages = ChatMessage.order(created_at: :asc).limit(50) 
+  
+	# Convert the message objects to a hash format for JSON response
+	messages.map { |message| 
+	  {
+		id: message.id,
+		sender_id: message.sender_id,
+		receiver_id: message.receiver_id,
+		content: message.content,
+		created_at: message.created_at
+	  }
+	}.to_json
+  end
+
+
+  options "*" do
+	response.headers["Allow"] = "GET, POST, OPTIONS"
+	response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+	response.headers["Access-Control-Allow-Origin"] = "http://127.0.0.1:5500" # remember to change this to the domain of website
+	response.headers["Access-Conrtol-Allow-Methods"] = "POST, GET, OPTIONS"
+	200
+end
---
+++ online chart/db_init.rb
@@ -0,0 +1,35 @@
+require 'active_record'
+require_relative 'models/chat_message'
+
+# Set up database connection
+ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  database: 'online_chat',
+  username: 'transcendence',
+  password: 'pass',
+  host: 'localhost'
+)
+
+# Test the database connection
+# begin
+#   puts "Testing database connection..."
+#   ActiveRecord::Base.connection
+
+#   if ActiveRecord::Base.connected?
+#     puts "Successfully connected to the database."
+#     puts "Trying to fetch the first chat message..."
+
+#     first_message = ChatMessage.first
+#     if first_message
+#       puts "First chat message content: #{first_message.content}"
+#     else
+#       puts "No chat messages found."
+#     end
+#   else
+#     puts "Failed to connect to the database."
+#   end
+# rescue ActiveRecord::NoDatabaseError => e
+#   puts "Database does not exist: #{e.message}"
+# rescue ActiveRecord::ActiveRecordError => e
+#   puts "Error connecting to the database: #{e.message}"
+# end
